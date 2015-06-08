560343710f2a79db764fa5026a4503c1af08cf29
AfirSraftGarrier <AfirSraftGarrier@gmail.com>
2015-06-08 16:32:33 +0800

    Update test file.

--- src/test/java/com/acc/java/AppTest.java
+++ src/test/java/com/acc/java/AppTest.java
@@ -24,20 +24,16 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-public class AppTest 
-    extends TestCase
-{
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+public class AppTest extends TestCase {
+	public AppTest(String testName) {
+		super(testName);
+	}
 
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	public static Test suite() {
+		return new TestSuite(AppTest.class);
+	}
+
+	public void testApp() {
+		assertTrue(true);
+	}
 }
