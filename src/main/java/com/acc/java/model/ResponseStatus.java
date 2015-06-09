967b593c2dd45a7d86a7dcd00def7b066fe0c15d
AfirSraftGarrier <AfirSraftGarrier@gmail.com>
2015-06-09 11:46:17 +0800

    Update Response class.

--- src/main/java/com/acc/java/model/ResponseStatus.java
+++ src/main/java/com/acc/java/model/ResponseStatus.java
@@ -19,41 +19,6 @@
  */
 package com.acc.java.model;
 
-public class Response<T> {
-	private ResponseStatus responseStatus;
-	private Integer tagCode;
-	private String description;
-	private T resultObject;
-
-	public T getResultObject() {
-		return resultObject;
-	}
-
-	public void setResultObject(T resultObject) {
-		this.resultObject = resultObject;
-	}
-
-	public ResponseStatus getResponseStatus() {
-		return responseStatus;
-	}
-
-	public void setResponseStatus(ResponseStatus responseStatus) {
-		this.responseStatus = responseStatus;
-	}
-
-	public Integer getTagCode() {
-		return tagCode;
-	}
-
-	public void setTagCode(Integer tagCode) {
-		this.tagCode = tagCode;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
+public enum ResponseStatus {
+	ILEGARG, EXCEPTION, FAIL, SUCCESS
 }
